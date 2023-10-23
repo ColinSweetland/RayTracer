b2337a074f02f624a1079564b49dcdeef4928796
Colin Sweetland <colinjswee@gmail.com>
2023-10-23 13:16:17 -0400

    Added Camera and cast first rays

--- src/Color.java
+++ src/Color.java
@@ -3,6 +3,22 @@ public class Color extends Vec3 {
     // r, g, and b are from 0.0 (black) to 1.0, translated to 0-255 only when needed
     public Color(double r, double g, double b) {
         super(r, g, b);
+        assert r() <= 1.0 && r() >= 0.0;
+        assert g() <= 1.0 && g() >= 0.0;
+        assert b() <= 1.0 && b() >= 0.0;
+    }
+
+    public Color(Vec3 v3) {
+        super(v3.x(), v3.y(), v3.z());
+        assert r() <= 1.0 && r() >= 0.0;
+        assert g() <= 1.0 && g() >= 0.0;
+        assert b() <= 1.0 && b() >= 0.0;
+    }
+
+    // lerp amount from 0.0-1.0. blends between c1 and c2
+    public static Color lerpBetween(Color c1, Color c2, double lerp_amount) {
+        assert lerp_amount <= 1.0 && lerp_amount >= 0.0;
+        return new Color(Vec3.add(Vec3.scalarMul((1 - lerp_amount), c1), Vec3.scalarMul(lerp_amount, c2)));
     }
 
     public double r() {
--- src/RayCaster.java
+++ src/RayCaster.java
@@ -1,9 +1,52 @@
 
 public class RayCaster {
+
+    // cast a ray and get the color it sees as a result. Currently hardcode black
+    public static Color castRay(Ray r) {
+        Vec3 unit = r.getDirection().toUnit();
+        double lerp_amount = 0.5 * (unit.y() + 1.0);
+        return Color.lerpBetween(new Color(1.0, 1.0, 1.0), new Color(0.5, 0.7, 1.0), lerp_amount);
+    }
+
     public static void main(String[] args) {
 
-        int image_width = 256;
-        int image_height = 256;
+        double ideal_aspect_ratio = 16.0 / 9.0;
+
+        int image_width = 400;
+        int image_height = (int) (image_width / ideal_aspect_ratio);
+
+        // ensure image height is at least one
+        image_height = Math.max(image_height, 1);
+
+        // Viewport is a "plane" in front of the camera where we cast our rays from
+        double viewport_height = 2.0;
+        double viewport_width = viewport_height * (double) (image_width / image_height);
+
+        // Camera will start at the origin, for now
+        // y axis is up, positive x is right, negative z is forward (right hand
+        // coordinates)
+        Vec3 camera_origin = new Vec3(0.0, 0.0, 0.0);
+
+        // distance between camera origin and viewport_width (in negative z direction)
+        Vec3 focal_length = new Vec3(0.0, 0.0, -1.0);
+
+        // Vector pointing from upper left of viewport to upper left.
+        Vec3 viewport_lr = new Vec3(viewport_width, 0.0, 0.0);
+        // Vector pointing from upper left of viewport to bottom left.
+        Vec3 viewport_ud = new Vec3(0.0, -viewport_height, 0.0);
+
+        // distance between pixels in viewport
+        Vec3 pixel_delta_lr = Vec3.scalarDiv(image_width, viewport_lr);
+        Vec3 pixel_delta_ud = Vec3.scalarDiv(image_height, viewport_ud);
+
+        // calculate viewport upper left
+        Vec3 viewport_upper_left = Vec3.add(camera_origin, focal_length);
+        viewport_upper_left = Vec3.sub(viewport_upper_left, Vec3.scalarDiv(2.0, viewport_lr));
+        viewport_upper_left = Vec3.sub(viewport_upper_left, Vec3.scalarDiv(2.0, viewport_ud));
+
+        // center of first pixel is offset half of each pixel delta from upper keft
+        Vec3 pixel_00_pos = Vec3.add(viewport_upper_left, Vec3.scalarDiv(2.0, pixel_delta_lr));
+        pixel_00_pos = Vec3.add(pixel_00_pos, Vec3.scalarDiv(2.0, pixel_delta_ud));
 
         // PPM magic number
         System.out.println("P3");
@@ -18,10 +61,14 @@ public class RayCaster {
             System.err.format("\rRendering %d/%d", y + 1, image_height);
 
             for (int x = 0; x < image_width; x++) {
-                // rg become % of x or y amount as decimal
-                Color pixelcol = new Color(((double) x) / (image_width - 1), ((double) y) / (image_height - 1), 0.0);
+                // shift to current pixel position
+                Vec3 pixel_pos = Vec3.add(pixel_00_pos, Vec3.scalarMul(x, pixel_delta_lr));
+                pixel_pos = Vec3.add(pixel_pos, Vec3.scalarMul(y, pixel_delta_ud));
 
-                Color.write(pixelcol);
+                Ray curr_ray = new Ray(camera_origin, Vec3.sub(pixel_pos, camera_origin));
+
+                Color pixeColor = castRay(curr_ray);
+                Color.write(pixeColor);
             }
         }
         System.err.println();
